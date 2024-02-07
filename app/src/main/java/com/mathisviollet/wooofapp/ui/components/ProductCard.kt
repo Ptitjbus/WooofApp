50cf54d7e694cbd64e183d44c941ecf1ef581f17
ptitjbus <ptitjbus02@gmail.com>
2024-02-07 11:45:38 +0100

    Add product rounded border

--- app/src/main/java/com/mathisviollet/wooofapp/ui/components/ProductCard.kt
+++ app/src/main/java/com/mathisviollet/wooofapp/ui/components/ProductCard.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -71,6 +72,7 @@ fun ProductCard(product : Product, onClick : () -> Unit) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(228.dp)
+                        .clip(RoundedCornerShape(12.dp))
                 )
                 Row(
                     modifier = Modifier
